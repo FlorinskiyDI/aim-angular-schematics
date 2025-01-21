bbfa69d976755af55d724e17673f106d173c2f67
FlorinskiyDI <dev.florynskyi@gmail.com>
2025-01-21 16:11:49 +0200

    aim-schematics-deploy: added schemantics project; renamed folder src to schematics.

---
+++ aim-schematics-deploy/library/schematics/library/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.library = library;
+// You don't have to export the function as default. You can also have more than one rule factory
+// per file.
+function library(_options) {
+    return (tree, _context) => {
+        return tree;
+    };
+}
+//# sourceMappingURL=index.js.map
